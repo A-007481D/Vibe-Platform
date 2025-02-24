79ef26fb717d75804877f65318350fcf6c6dd360
A-007481D <labidabdelmalek@gmail.com>
2025-02-24 00:26:52 +0000

    changes : model modifications to support UUIDs & timestamps for friend requests table

--- app/Models/FriendRequest.php
+++ app/Models/FriendRequest.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FriendRequest extends Model
 {
+    use HasFactory, HasUuids;
+    protected string $keytype = 'string';
+    public $incrementing = false;
     protected $fillable = ['sender_id', 'receiver_id', 'status'];
+    public $timestamps = true;
 
     public function sender()
     {
