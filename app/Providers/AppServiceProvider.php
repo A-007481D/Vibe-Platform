20c351aba7917c068b639008d821d4df48e9ef0d
A-007481D <labidabdelmalek@gmail.com>
2025-02-25 12:19:54 +0000

    Notifications : new migration and AppServiceProvider update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Services\NotificationService;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('*', function ($view) {
+            if (auth()->check()) {
+                $notifications = app(NotificationService::class)->getUnreadNotifications();
+                $view->with('notifications', $notifications);
+            }
+        });
     }
 }
