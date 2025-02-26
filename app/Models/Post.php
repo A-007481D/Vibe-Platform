284274ce3601c7123518701683e5339bfc2b1200
A-007481D <labidabdelmalek@gmail.com>
2025-02-26 16:07:08 +0000

    posts : post model

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $fillable = ['id','user_id', 'content', 'image_url'];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes() {
+        return $this->hasMany(Like::class);
+    }
+
+    public function shares(){
+        return $this->hasMany(Share::class);
+    }
+}
