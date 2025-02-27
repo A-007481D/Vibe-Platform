645a7afbed87b55b2631d34821f6adc5ab67b150
A-007481D <labidabdelmalek@gmail.com>
2025-02-27 15:23:10 +0000

    likes : Updated the User & Post model to have the like relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -134,4 +134,10 @@ class User extends Authenticatable
         return $this->friends()->where('friend_id', $user->id)->exists();
     }
 
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+
 }
