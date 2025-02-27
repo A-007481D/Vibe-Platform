ca7f23bfa4d70b52d8ef2faabec1acc0d8ac576d
A-007481D <labidabdelmalek@gmail.com>
2025-02-27 15:19:18 +0000

    likes : added the likes controller

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\Like;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    //
+    public function toggleLike(Request $request, Post $post)
+    {
+        $user = auth()->user();
+
+        if (!$user) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $like = $post->likes()->where('user_id', $user->id)->first();
+        if ($like) {
+            $like->delete();
+        } else {
+            $post->likes()->create(['user_id' => $user->id]);
+        }
+
+        $post->load('likes');
+
+        return view('components.like-button', ['post' => $post]);
+    }
 }
